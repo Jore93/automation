609ff086403c312a5411ef00cbd7a84161ff212b
JPR <18405900+Jore93@users.noreply.github.com>
2025-05-27 22:54:37 +0300

    Updated provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,23 @@
-provider "proxmox" {
-  pm_api_url      = "https://pve.home.lan:8006/api2/json"
-  pm_tls_insecure = true
+terraform {
+  required_version = ">= 0.14"
+  required_providers {
+    proxmox = {
+      source = "bpg/proxmox"
+    }
+  }
+}
 
+variable "api_token" {}
+variable "proxmox_endpoint" {}
+
+provider "proxmox" {
+  endpoint      = var.proxmox_endpoint
+  insecure      = true
+  api_token     = var.api_token
   ssh {
-    agent    = true
+    agent = false
     username = "root"
+    private_key = file("~/.ssh/proxmox")
   }
 }
 
